2f3085aae7b317311d4d0f94f575d4578bff2275
Jonathan <JonathanWeiner@quickenloans.com>
2021-05-25 10:26:46 -0400

    first commit

---
+++ Coffee_Shop/Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Coffee_Shop.Models
+{
+    public class UserModel
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
